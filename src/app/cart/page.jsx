d58d364074769945c8964ae22759a129d5a97fb2
meherunnesaenta <meherunnesaenta1@gmail.com>
2026-03-22 19:03:49 +0600

    cart added

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,11 +1,9 @@
 import { getCart } from '@/actions/server/cart';
 
 import Cart from '@/components/home/Cart';
-import { useSession } from 'next-auth/react';
 import React from 'react';
 
 const page = async () => {
-    const session = useSession();
     const carts = await getCart();
     console.log(carts)
   return (
--- src/components/home/Cart.jsx
+++ src/components/home/Cart.jsx
@@ -1,13 +1,20 @@
 'use client'
 import React, { useMemo, useState } from 'react';
 import CartItem from '../card/CartItem';
+import { usePathname, useRouter } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
 const Cart = ({ cart = [] }) => {
+    const router =useRouter();
+    const path =usePathname();
+    const sesssion= getServerSession();
     const [item, setItem] = useState(cart);
 
     const totalItems = useMemo(() => item.reduce((acm, item) => acm + item.quantity, 0), [item]);
 
-    
+    if(!sesssion){
+        router.push(`/login?callbackUrl=${path}`);
+    }
     return (
         <div>
             <h2> total items : {totalItems}</h2>
